56cc8076847197e7ebe758e5e72f91e79f0426ad
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2020-09-30 12:18:20 -0700

    Updated scripts

--- new-release.js
+++ new-release.js
@@ -1,4 +1,4 @@
-
+// TODO Diffs need to ignore pfx files and android/ios dirs
 
 const newRelease = '0.63.3';
 const rnVersion = '^0.63';
@@ -24,6 +24,8 @@ execSync(`git checkout -b "${branchName}"`);
 execSync(`npx react-native init "${appName}" --template react-native@${rnVersion}`);
 execSync(`cd ${appName}`);
 execSync(`npx react-native-windows-init --version ${newRelease} --overwrite`);
+// Modify some files to prevent new guids being generated and showing up in the diffs
+execSync(`node standardizeProj.js`);
 execSync(`cd ..`);
 execSync(`git add ${appName}`);
 execSync(`git commit -m "Release cpp/${newRelease}"`);
---
+++ standardizeProj.js
@@ -0,0 +1,54 @@
+// @ts-check
+
+const { readFileSync, writeFileSync } = require("fs");
+// We always use this GUID to represent the apps project guid
+const standardAppProjectGuid = "{UNIQUE00-APP0-GUID-GOES-HERE00000000}";
+
+const appName = "RnDiffApp";
+let projectGuid = "";
+
+function standardizeSolutionFile() {
+  const slnFilePath = `./${appName}/windows/${appName}.sln`;
+  const sln = readFileSync(slnFilePath);
+  const reg = new RegExp(
+    `Project.*"${appName}", "${appName}\\\\${appName}.vcxproj", "(.*)"`,
+    "m"
+  );
+  const matches = reg.exec(sln.toString());
+
+  projectGuid = matches[1];
+
+  if (!projectGuid) {
+    throw new Error("Failed to find app projects guid");
+  }
+
+  // Replace all the guids with the standard one
+  writeFileSync(
+    slnFilePath,
+    sln.toString().replace(new RegExp(projectGuid, "g"), standardAppProjectGuid)
+  );
+}
+
+function standardizeAppsProjectFile() {
+  const appProjPath = `./${appName}/windows/${appName}/${appName}.vcxproj`;
+  const appProj = readFileSync(appProjPath);
+
+  writeFileSync(
+    appProjPath,
+    appProj
+      .toString()
+      .replace(
+        new RegExp(`<ProjectGuid>.*</ProjectGuid>`),
+        `<ProjectGuid>${standardAppProjectGuid}</ProjectGuid>`
+      )
+      .replace(
+        new RegExp(
+          `<PackageCertificateThumbprint>.*</PackageCertificateThumbprint>`
+        ),
+        "<PackageCertificateThumbprint>UniqueThumbPrintHere</PackageCertificateThumbprint>"
+      )
+  );
+}
+
+standardizeSolutionFile();
+standardizeAppsProjectFile();
