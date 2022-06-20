a1f6323ba5eb31a4c1be855e299abb862a22250f
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2022-06-20 12:41:59 -0700

    Release cpp/0.66.19

---
+++ RnDiffApp/windows/rndiffapp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::rndiffapp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::rndiffapp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
